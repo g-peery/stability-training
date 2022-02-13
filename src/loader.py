ff159367007435684bb79bb54e12024a2759f6a9
Ahmed Baruwa <aobaruwa@yahoo.com>
2022-02-13 14:09:29 -0800

    updated loader.py

--- src/loader.py
+++ src/loader.py
@@ -2,6 +2,7 @@
 import os 
 import torch
 import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
 from torchvision import transforms as T
 
 # Adapted from https://github.com/tjmoon0104/pytorch-tiny-imagenet/blob/master/AlexNet.ipynb
@@ -26,7 +27,11 @@ num_workers = {
 image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
                                           data_transforms[x])
                   for x in ['train', 'val','test']}
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=512,
+dataloaders = {x: DataLoader(image_datasets[x], batch_size=512,
                                              num_workers=num_workers[x], shuffle=True)
               for x in ['train', 'val', 'test']}
 dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val', 'test']}
+## Add 
+val_datasets = datasets.ImageFolder(os.path.join(data_dir, 'val'),
+                                          data_transforms['val'])
+val_ldr = DataLoader(val_datasets, batch_size=128, num_workers=16, shuffle=True)
